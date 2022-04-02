47ad468e8a003608af1635a4725c8b45726169d1
Yaniv Blum <jungob94@gmail.com>
2022-04-02 18:40:30 +0300

    fixing review

--- src/components/Reviews/ReviewItem.js
+++ src/components/Reviews/ReviewItem.js
@@ -5,21 +5,36 @@ const ReviewItem = (props) => {
   return (
     <li>
       <Card className="review-item">
-        <div className="review-item__description">
-          <h2> קורס:{props.courseName}</h2>
-          </div>
-          <div className="review-item__price">
+        <div className="item__ratingValues">
+        <div className="review-item__ratingValuesCard">
             דירוג כללי: {props.totalRating}{" "}
+            </div>
+            <div className="review-item__ratingValuesCard">
             דרגת קושי: {props.difficulty}{" "}
           </div>
-          <div className="review-item__price">
-          <h2>חובת הגשות: {props.hwReq}</h2>
-          <h2>מטלות ממוחזרות: {props.hwRef}</h2>
-          <h2>מעלה חומרים למודל: {props.moodleQuality}</h2>
-          <h2>קיימות הקלטות: {props.records}</h2>
           </div>
-          <div className="review-item__price">
-          <h2>הערות: {props.textReview}</h2>
+
+          <div className="review-item__ratingInfo">
+            <div className="review-item__header">
+              {props.courseName}
+              </div>
+              <div className="review-item__courseMeta">
+                <div className="review-item__metaItem">
+         חובת הגשות: {props.hwReq}
+         </div>
+         <div className="review-item__metaItem">
+         מטלות ממוחזרות: {props.hwRef}  
+         </div>
+         <div className="review-item__metaItem">
+          מעלה חומרים למודל: {props.moodleQuality}  
+          </div>
+          <div className="review-item__metaItem">
+          קיימות הקלטות: {props.records}
+          </div>
+          </div>
+          <div className="review-item__comment">
+          הערות: {props.textReview}
+        </div>
         </div>
       </Card>
     </li>
