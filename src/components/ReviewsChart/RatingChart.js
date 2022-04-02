43927fa7237740835661696965e8260fa2a32264
Yaniv Blum <jungob94@gmail.com>
2022-04-02 17:39:58 +0300

    added fonts

---
+++ src/components/ReviewsChart/RatingChart.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import ChartBar from './ChartBar';
+import './RatingChart.css';
+
+
+// edit props 
+const RatingChart = (props) => {
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
+  return (
+    <div className='chart'>
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RatingChart;
