a5e130db1ea3dc5a2afda80dee05d5bed565fb10
Yaniv Blum <jungob94@gmail.com>
2022-04-02 15:23:29 +0300

    started reviews list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import Reviews from './components/Reviews/Reviews'
+const expenses = [
+  {
+    id: 'r1',
+    totalRating: 5,
+    difficulty: 3,
+    courseName: 'מבוא למדמח',
+    hwReq: 'כן',
+    hwRef: 'לא',
+    moodleQuality: 'כן',
+    records: 'כן',
+    textReview:'אחלה מרצה, מלמד מעולה, מטלות לא קלות ואין רפרנס לרובן',
+  },
+  {
+    id: 'r2',
+    totalRating: 3,
+    difficulty: 5,
+    courseName: 'מבני נתונים',
+    hwReq: 'כן',
+    hwRef: 'לא',
+    moodleQuality: 'כן',
+    records: 'כן',
+    textReview:'מבחן קשה מאוד בנוסף למטלות לא קלות ואין רפרנס לרובן',
+  },
+];
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Reviews items = {expenses}/>
     </div>
   );
 }
