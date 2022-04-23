236c03eb9fd812bd16a499e6470bbc6a11d56311
Maria Novizky <marianovizky@gmail.com>
2022-04-23 18:47:51 +0300

    maria - improvements

--- src/pages/addReview/components/ChoiceSegment.js
+++ src/pages/addReview/components/ChoiceSegment.js
@@ -4,9 +4,8 @@ import CustomRadioButton from './CustomRadioButton';
 const ChoiceSegment = (props) => {
     return (
         <div className='choice-segment_options'>
-            <CustomRadioButton onChoosingOption={props.onChoosingOption} name={props.name} value={props.values[0]}></CustomRadioButton>
-            <CustomRadioButton onChoosingOption={props.onChoosingOption} name={props.name} value={props.values[1]}></CustomRadioButton>
-            <CustomRadioButton onChoosingOption={props.onChoosingOption} name={props.name} value={props.values[2]}></CustomRadioButton>
+            <CustomRadioButton onChoosingOption={props.onChoosingOption} name={props.name} value={props.values[0]} label={props.labels[0]}></CustomRadioButton>
+            <CustomRadioButton onChoosingOption={props.onChoosingOption} name={props.name} value={props.values[1]} label={props.labels[1]}></CustomRadioButton>
         </div>
     )
 }
---
+++ src/pages/addReview/components/DividedCard.js
@@ -0,0 +1,17 @@
+import './DividedCard.css'
+
+const DividedCard = (props) => {
+    return (
+        <div className="divided-card">
+            <div className='divided-card_1sthalf'>
+                {props.children[0]}
+            </div>
+            <div className="vl"></div>
+            <div className='divided-card_2ndhalf'>
+                {props.children.slice(1)}
+            </div>
+        </div>
+    )
+}
+
+export default DividedCard;
