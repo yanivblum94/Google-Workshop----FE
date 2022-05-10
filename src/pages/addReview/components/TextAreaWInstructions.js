394345a03554c5c392dfa56458b5c30c6cd41450
Almog Ben‐Yochanan <benyochanan@mail.tau.ac.il>
2022-05-11 01:47:26 +0300

    rephrasing

--- src/pages/addReview/components/TextAreaWInstructions.js
+++ src/pages/addReview/components/TextAreaWInstructions.js
@@ -7,14 +7,14 @@ const TextAreaWInstrucions = (props) => {
             <DividedDiv>
                 <div className="free-input_instructions__1st">
                     <div className="free-input_instructions__titles">כן</div>
-                    <div className="free-input_instructions__texts">לכתוב בהרחבה איך היה המרצה, מה אהבתם ומה פחות</div>
+                    <div className="free-input_instructions__texts">לכתוב בהרחבה איך היה המרצה, מה אהבתם וּמה פחות</div>
                 </div>
                 <div className="free-input_instructions__2nd">
                     <div className="free-input_instructions__titles">לא</div>
-                    <div className="free-input_instructions__texts">להשתמש בשפה לא נאותה ובמילים לא מכבדות</div>
+                    <div className="free-input_instructions__texts">להשתמש בשפה שאינה נאותה או במילים שאינן מכבדות</div>
                 </div>
             </DividedDiv>
-            <textarea onChange={props.onWritingReview} className='free-input_box' rows={10} MaxLength={300} placeholder={"אנא הקלידו תגובה באורך מינימום 20 תווים"}></textarea>
+            <textarea onChange={props.onWritingReview} className='free-input_box' rows={10} MaxLength={300} placeholder={"התגובה צריכה להכיל 20 תווים לפחות"}></textarea>
         </ColumnCard>
     )
 }
