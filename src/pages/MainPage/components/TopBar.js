8dd0019e87710010b90e37108b2899c0d56c3a22
Gai Greenberg <gaigreenberg2@gmail.com>
2022-05-25 14:13:15 +0300

    add link to main page from top bar logo

--- src/pages/MainPage/components/TopBar.js
+++ src/pages/MainPage/components/TopBar.js
@@ -2,10 +2,12 @@ import pic from "../images/logo.png";
 import GoogleButton from "react-google-button";
 import "./TopBar.css";
 import { initializeApp } from "firebase/app";
-import { useState, useRef, useContext } from 'react';
-import AuthContext from '../../../store/auth-context';
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../../store/auth-context";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { firebaseConfig } from "../firebaseConfig";
+import { Link } from "react-router-dom";
+
 
 
 const app = initializeApp(firebaseConfig);
@@ -13,11 +15,10 @@ export const auth = getAuth(app);
 const TopBar = () => {
   const authCtx = useContext(AuthContext);
 
-  const SignInWithGoogle =() => {
-    
+  const SignInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({
-      hd: "mail.tau.ac.il"
+      hd: "mail.tau.ac.il",
     });
     signInWithPopup(auth, provider)
       .then((result) => {
@@ -27,15 +28,19 @@ const TopBar = () => {
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
   return (
     <div className="upper-bar">
-      <img src={pic} className="logo-pic" />
-      {!authCtx.isLoggedIn &&<GoogleButton
-        className="google-button"
-        type="light"
-        onClick={SignInWithGoogle}
-      />}
+      <Link to="/mainPage">
+        <img src={pic} className="logo-pic" />
+      </Link>
+      {!authCtx.isLoggedIn && (
+        <GoogleButton
+          className="google-button"
+          type="light"
+          onClick={SignInWithGoogle}
+        />
+      )}
       {authCtx.isLoggedIn && <p>hi {authCtx.userName}</p>}
     </div>
   );
