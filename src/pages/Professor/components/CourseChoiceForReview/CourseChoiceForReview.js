85b0eeccf55c38ee3e21dac938495b77379a51b4
Maria Novizky <marianovizky@gmail.com>
2022-08-21 05:54:46 +0300

    prof course choice default value

--- src/pages/Professor/components/CourseChoiceForReview/CourseChoiceForReview.js
+++ src/pages/Professor/components/CourseChoiceForReview/CourseChoiceForReview.js
@@ -10,7 +10,7 @@ const CourseChoiceForReview = (props) => {
 
     return (
         <div className='course-choice-for-review'>
-            <Select onChange={courseChangeHandler} placeholder="אנא בחרו קורס" className="course-choice-review_select" options={props.course_options}></Select>
+            <Select onChange={courseChangeHandler} defaultValue={props.course_options[0]} placeholder="אנא בחרו קורס" className="course-choice-review_select" options={props.course_options}></Select>
         </div>
     );
 }
